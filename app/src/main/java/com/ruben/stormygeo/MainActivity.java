3225c9ecee1a4b2a126d20510c0b3ed13d4d3590
Ruben Vilchez <rd.vilchez@gmail.com>
2018-01-29 16:57:19 -0500

    Configuracion OkHttp

--- app/src/main/java/com/ruben/stormygeo/MainActivity.java
+++ app/src/main/java/com/ruben/stormygeo/MainActivity.java
@@ -1,13 +1,68 @@
 package com.ruben.stormygeo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String apiKey = "4c34a5e97de8707b362a05691b22f49b";
+        double latitude = 4.598889;
+        double longitude = -74.080833;
+        String foreCastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(foreCastUrl)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.v(TAG, response.body().string());
+                }
+            }
+        });
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
